4411111cab12d1cf22894e483321ff568e86b58b
Dave Lockwood <dave.r.lockwood@gmail.com>
2024-03-07 19:31:39 -0500

    fix myst

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Open Streaming Platform'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser']
+extensions = ['myst-parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
